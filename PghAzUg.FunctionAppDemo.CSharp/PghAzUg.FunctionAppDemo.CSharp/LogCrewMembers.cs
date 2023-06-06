3f9c8c90520177a7c153bd909ec23d182c58a4ed
Lorne Hubenthal <lorne@lorne.dev>
2023-06-06 05:07:40 -0400

    Add in Service Bus Queue Processing

---
+++ PghAzUg.FunctionAppDemo.CSharp/PghAzUg.FunctionAppDemo.CSharp/LogCrewMembers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using PghAzUg.FunctionAppDemo.CSharp.Models;
+
+namespace PghAzUg.FunctionAppDemo.CSharp;
+
+public static class LogCrewMembers
+{
+   
+    [Function("LogCrewMembers")]
+    public static LogCrewMemberResponse Run([ServiceBusTrigger("process-entity", Connection = "AzureServiceBus")] EntityMessage message, FunctionContext context)
+    {
+        ILogger logger = context.GetLogger("LogCrewMembers");
+        logger.LogInformation("C# ServiceBus queue trigger function processed message for Job ID: {JobID} Crew Name: {Name}", message.JobId, message.Data?.Name);
+
+        return new LogCrewMemberResponse { CrewMember = message.Data };
+    }
+}
---
+++ PghAzUg.FunctionAppDemo.CSharp/PghAzUg.FunctionAppDemo.CSharp/Models/EntityMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace PghAzUg.FunctionAppDemo.CSharp.Models;
+
+public class EntityMessage
+{
+    public Guid JobId { get; set; }
+    public Entity? Data { get; set; }
+}
+
+public class Entity
+{
+    public string? Name { get; set; }
+    public string? Rank { get; set; }
+    public string? Position { get; set; }
+    public string? Species { get; set; }
+    public bool? EatenByArmus { get; set; }
+}
